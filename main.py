e54e5ac384c1bbf5539944783ca9bd69ecadceda
nwizugbesamson <samson6398@gmail.com>
2022-05-27 22:58:29 +0100

    update postgres requirement

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 Bootstrap(app)
 
 # Initialize and configure sqlalchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL'), 'sqlite:///projects.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///projects.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
